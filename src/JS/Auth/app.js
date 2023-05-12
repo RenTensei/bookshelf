95fcd6373ee5e6bc24eaea9abb14233642887798
RenTensei <scs.bon7@gmail.com>
2023-05-12 18:11:29 +0300

    firestore

--- src/JS/Auth/app.js
+++ src/JS/Auth/app.js
@@ -11,7 +11,18 @@ import {
   onAuthStateChanged,
   signOut,
 } from 'firebase/auth';
-import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  doc,
+  setDoc,
+  getDoc,
+  updateDoc,
+  arrayUnion,
+  FieldValue,
+} from 'firebase/firestore/lite';
+
 import { Notify } from 'notiflix';
 import { signedUserUI } from './signedUserUI';
 import { unsignedUserUI } from './unsignedUserUI';
@@ -20,16 +31,6 @@ import { closeModalStart } from '../modal-start';
 Notify.init({
   position: 'center-top',
 });
-
-// Get a list of books from your database
-const db = getFirestore(firebase);
-
-async function getBooks(db) {
-  const booksCol = collection(db, 'books');
-  const bookSnapshot = await getDocs(booksCol);
-  const bookList = bookSnapshot.docs.map(doc => doc.data());
-  return bookList;
-}
 
 // Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(firebase);
@@ -47,6 +48,8 @@ logOutBtnMobile.addEventListener('click', signOutUser);
 onAuthStateChanged(auth, user => {
   if (user != null) {
     console.log('user logged in');
+    console.log(user.uid);
+
     signedUserUI();
     return;
   }
@@ -82,13 +85,13 @@ function handleSignUp(e) {
 
   console.log(name, email, password);
 
-  signUp(email, password);
+  signUp(email, password, name);
 }
 
 function handleSignIn(e) {
   e.preventDefault();
 
-  const name = signInForm.elements.name.value;
+  // const name = signInForm.elements.name.value;
   const email = signInForm.elements.email.value;
   const password = signInForm.elements.password.value;
 
@@ -97,11 +100,19 @@ function handleSignIn(e) {
   signIn(email, password);
 }
 
-export function signUp(email, password) {
+export function signUp(email, password, username) {
   createUserWithEmailAndPassword(auth, email, password)
     .then(userCredential => {
       // Handle successful sign-up
       const user = userCredential.user;
+
+      try {
+        const userRef = doc(db, 'users', user.uid);
+        setDoc(userRef, { email, username });
+      } catch (error) {
+        console.log(error);
+      }
+
       Notify.success('Signed up!');
       closeModalStart();
     })
@@ -137,4 +148,114 @@ export function signOutUser() {
       Notify.failure(error.code);
       // console.error('Logout error:', error);
     });
+}
+
+// firestore -----------------------------------------
+
+// Get a list of books from your database
+const db = getFirestore(firebase);
+
+export async function addBookToFirestore(bookInfo) {
+  const user = auth.currentUser;
+  if (user) {
+    const userId = user.uid;
+    const userRef = doc(db, 'users', userId);
+
+    // Check if the document exists
+    const userDoc = await getDoc(userRef);
+    if (userDoc.exists()) {
+      // Check if the 'books' field exists
+      const userData = userDoc.data();
+      if (userData.books) {
+        // Update the 'books' field using arrayUnion
+        await updateDoc(userRef, {
+          books: arrayUnion(bookInfo),
+        });
+      } else {
+        // Create the 'books' field and set it as an array containing the book name
+        await setDoc(
+          userRef,
+          {
+            books: [bookInfo],
+          },
+          { merge: true }
+        );
+      }
+      console.log('Book added to shopping list:', bookInfo);
+    } else {
+      // Create the user document and add the book to the 'books' field
+      await setDoc(userRef, {
+        books: [bookInfo],
+      });
+      console.log(
+        'New user document created with book added to shopping list:',
+        bookInfo
+      );
+    }
+  }
+}
+
+export async function removeBookFromFirestore(book) {
+  const user = auth.currentUser;
+  if (user) {
+    const userId = user.uid;
+    const userRef = doc(db, 'users', userId);
+
+    updateDoc(userRef, {
+      books: firebase.firestore.FieldValue.arrayRemove(book),
+    })
+      .then(() => {
+        console.log('Book removed from shopping list:', book);
+      })
+      .catch(error => {
+        console.error('Error removing book from shopping list:', error);
+      });
+  }
+}
+
+export async function removeBookFromShoppingList(bookId) {
+  const user = auth.currentUser;
+  if (user) {
+    const userId = user.uid;
+    const userRef = doc(db, 'users', userId);
+
+    const userDoc = await doc.get(userRef);
+    if (userDoc.exists()) {
+      const userData = userDoc.data();
+      const updatedBooks = userData.books.filter(book => book._id !== bookId);
+
+      await updateDoc(userRef, {
+        books: updatedBooks,
+      });
+
+      console.log('Book removed from shopping list:', bookId);
+    } else {
+      console.error('User document does not exist');
+    }
+  }
+}
+
+export function removeBookById(bookId) {
+  const user = auth.currentUser;
+  if (user) {
+    const userId = user.uid;
+    const userRef = doc(db, 'users', userId);
+
+    getDoc(userRef)
+      .then(userDoc => {
+        if (userDoc.exists()) {
+          const userData = userDoc.data();
+          const books = userData.books || [];
+
+          books.forEach(book => {
+            if (book.id === bookId) {
+              removeBookFromShoppingList(bookId);
+            }
+          });
+        }
+      })
+      .catch(error => {
+        console.error('Error fetching user data:', error);
+      });
+  }
 }
