6665b2417ed7a936fb7d1907074b62780992902f
RenTensei <scs.bon7@gmail.com>
2023-05-14 23:04:42 +0300

    fix switcher

--- src/JS/header.js
+++ src/JS/header.js
@@ -1,44 +1,22 @@
-import { getUserData } from './autorisation';
-import { clearUserData } from './autorisation';
-import { parsData } from './autorisation';
-
 const burgerMenu = document.querySelector('.modal-mobile-menu');
 const btnOpen = document.querySelector('.header-burger');
 const closeModalMenu = document.querySelector('.mobile-close-btn');
-
-export const checkbox = document.querySelector('.theme-switch');
+export const checkbox = document.querySelector('#switch');
+export const checkboxMob = document.querySelector('#mob-switch');
 export const colorTheme = localStorage.getItem('ui-theme');
 
-const headerNav = document.querySelector('.header-nav');
-const userBar = document.querySelector('.dropdown');
-const btnSignup = document.querySelector('.btn-signup');
-const btnLogout = document.querySelector('.btn-logout');
-
-// //__________________________ isNonAutorised User  --  Hidden nav menu
-// (function checkUserKey() {
-//   if (getUserData()) {
-//     headerNav.classList.remove('is-hidden');
-//     userBar.style.display = 'flex';
-//     btnSignup.style.display = 'none';
-//   } else {
-//     headerNav.classList.add('is-hidden');
-//     userBar.style.display = 'none';
-//     btnOpen.style.display = 'none';
-//     btnSignup.style.display = 'flex';
-//   }
-// })();
-
-// //
-// (function checkColorTheme() {
-//   if (!colorTheme) {
-//     localStorage.setItem('ui-theme', 'light');
-//   }
-//   if (colorTheme === 'light') {
-//     checkbox.attributes.type.ownerElement.checked = false;
-//   } else {
-//     checkbox.attributes.type.ownerElement.checked = true;
-//   }
-// })();
+(function checkColorTheme() {
+  if (!colorTheme) {
+    localStorage.setItem('ui-theme', 'light');
+  }
+  if (colorTheme === 'light') {
+    checkbox.attributes.type.ownerElement.checked = false;
+    checkboxMob.attributes.type.ownerElement.checked = false;
+  } else {
+    checkbox.attributes.type.ownerElement.checked = true;
+    checkboxMob.attributes.type.ownerElement.checked = true;
+  }
+})();
 
 btnOpen.addEventListener('click', () => {
   burgerMenu.style.display = 'block';
@@ -48,41 +26,24 @@ closeModalMenu.addEventListener('click', () => {
   burgerMenu.style.display = 'none';
 });
 
-// checkbox.addEventListener('click', toggleCheckbox);
+checkbox.addEventListener('click', toggleCheckbox);
+checkboxMob.addEventListener('click', toggleCheckboxMob);
 
-// function toggleCheckbox() {
-//   if (!checkbox.attributes.type.ownerElement.checked) {
-//     localStorage.setItem('ui-theme', 'light');
-//   } else {
-//     localStorage.setItem('ui-theme', 'dark');
-//   }
-// }
-
-// //   UserBar
-// //--------------------------------------------------
-// // устанавливает имя полбзователя на кнопку
-// try {
-//   userBar.children[0].children[1].textContent = parsData.userName;
-// } catch (error) {
-//   // console.error(error);
-// }
-
-userBar.addEventListener('click', myFunction);
-// открывает выпадающее меню из кнопки
-function myFunction() {
-  document.getElementById('myDropdown').classList.toggle('show');
+function toggleCheckbox() {
+  if (!checkbox.attributes.type.ownerElement.checked) {
+    localStorage.setItem('ui-theme', 'light');
+    checkboxMob.attributes.type.ownerElement.checked = false;
+  } else {
+    localStorage.setItem('ui-theme', 'dark');
+    checkboxMob.attributes.type.ownerElement.checked = true;
+  }
 }
-
-// // Закрывает выпадающее меню, если пользователь щелкает за его пределами
-// window.onclick = function (event) {
-//   if (!event.target.matches('.dropbtn')) {
-//     var dropdowns = document.getElementsByClassName('dropdown-content');
-//     var i;
-//     for (i = 0; i < dropdowns.length; i++) {
-//       var openDropdown = dropdowns[i];
-//       if (openDropdown.classList.contains('show')) {
-//         openDropdown.classList.remove('show');
-//       }
-//     }
-//   }
-// };
+function toggleCheckboxMob() {
+  if (!checkboxMob.attributes.type.ownerElement.checked) {
+    localStorage.setItem('ui-theme', 'light');
+    checkbox.attributes.type.ownerElement.checked = false;
+  } else {
+    localStorage.setItem('ui-theme', 'dark');
+    checkbox.attributes.type.ownerElement.checked = true;
+  }
+}
